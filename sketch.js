9be45598c70d1cb93cb2dc019860aa6282399096
Lucas Reeves <lreeves0000@gmail.com>
2024-07-30 13:16:50 -0700

    window resize function added

--- sketch.js
+++ sketch.js
@@ -9,21 +9,6 @@ function setup() {
   }
 }
 
-function resize(){
-	resizeCanvas(windowWidth, windowHeight);
-	let new_star_amount = int((windowWidth*windowHeight) * (80/(600 ** 2)));
-	let curr_length = stars.length;
-	if(new_star_amount > curr_length){
-		for(let i = new_star_amount; i < curr_length; i++){
-			stars.push(new Star());
-		}
-	}else{
-		for(let i = new_star_amount; i < curr_length; i++){
-			stars.pop();
-		}
-	}
-}
-
 function draw() {
   resize();
   background(0);
@@ -32,3 +17,13 @@ function draw() {
     star.display();
   }
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); // Resize canvas when window size changes
+  stars_amount = int((windowWidth * windowHeight) * (80 / (600 ** 2)));
+  stars = []; // Clear existing stars
+  for (let i = 0; i < stars_amount; i++) {
+    stars.push(new Star()); // Recreate stars
+  }
+}
+
